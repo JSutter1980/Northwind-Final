25ff70f4a8f3adbef98ac31569bd36cbc9abb765
JSutter1980 <jsutter6@my.wctc.edu>
2023-12-07 19:43:49 -0600

    {1.02} Added cart items

---
+++ Controllers/CartController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class CartController : Controller
+{
+  // this controller depends on the NorthwindRepository
+  private DataContext _dataContext;
+  public CartController(DataContext db) => _dataContext = db;
+  public IActionResult Index() => View(_dataContext.CartItems.Include("Product").OrderBy(c => c.CartItemId));
+}
